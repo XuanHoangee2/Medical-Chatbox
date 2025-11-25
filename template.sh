ef550fbe6dbe447ab5c01451bbc3fcf0981b95d8
Xuan Hoang <tranxuanhoang14062004@gmail.com>
2025-11-25 15:29:07 +0700

    folder structure added

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p research
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch research/__init__.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirement.txt
+
+echo "Directory and files created successfully."
